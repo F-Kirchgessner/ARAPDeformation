bd802c66c7c951f860230f9c647deb5c5b4a4a4a
ate <ate@ate.de>
2018-01-22 13:14:27 +0100

    +global.h

---
+++ Simulations/global.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define MASS_POINT_SIZE 0.01f
+#define VELOCITY_MIN_SQ 0.000001f
+#define GROUND_HEIGHT -0.5f
+#define GROUND_DAMPING 0.5f
+#define BOX_SIZE 0.5f
+#define SPRING_LENGTH_MIN 0.000001f
