3d5e156d21e5eeabf67db5dc5b54c50ab6728662
ate <ate@ate.de>
2018-01-22 13:41:39 +0100

    +spring masspoint to

--- Simulations/Spring.h
+++ Simulations/Spring.h
@@ -1,17 +1,18 @@
 #pragma once
 #include "Simulator.h"
-#include "Masspoint.h"
+#include "RigidbodySystem.h"
+#include "global.h"
 
 class Spring
 {
 public:
 	Spring();
-	Spring(Masspoint& point1, Masspoint& point2, float stiffness, float initialLength);
+	Spring(RigidbodySystem& point1, RigidbodySystem& point2, float stiffness, float initialLength);
 	~Spring();
 
 public:
-	Masspoint *mass_point1;
-	Masspoint *mass_point2;
+	RigidbodySystem *mass_point1;
+	RigidbodySystem *mass_point2;
 	float stiffness;
 	float initialLength;
 	float currentLength;
@@ -21,13 +22,13 @@ public:
 	void computeElasticForces();
 	void addToEndPoints();
 
-	void setPoint1(Masspoint *point);
-	void setPoint2(Masspoint *point);
+	void setPoint1(RigidbodySystem *point);
+	void setPoint2(RigidbodySystem *point);
 	void setStiffness(float stiff);
 	void setInitalLength(float initlength);
 
-	Masspoint *getPoint1();
-	Masspoint *getPoint2();
+	RigidbodySystem *getPoint1();
+	RigidbodySystem *getPoint2();
 	float getStiffness();
 	float getInitialLength();
 };
