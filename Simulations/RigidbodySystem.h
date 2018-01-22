22d5310fa94f707fd699a0097391b144855c28d6
F-Kirchgessner <florian.kirchgessner@tum.de>
2018-01-22 14:26:45 +0100

    Initial RigidbodySystem for all physics based tasks

--- Simulations/RigidbodySystem.h
+++ Simulations/RigidbodySystem.h
@@ -5,57 +5,51 @@
 #include <DirectXMath.h>
 #include "collisionDetect.h"
 
+#define EULER 0
+#define LEAPFROG 1
+#define MIDPOINT 2
 
 class RigidbodySystem
 {
 public:
 	RigidbodySystem();
-	RigidbodySystem(Vec3 size, Vec3 position, float mass);
 	~RigidbodySystem();
 
-	// x_cm
-	Vec3 m_position;
-	// Translation Matrix
-	Mat4 transMat;
+	// Functions
+	void reset();
+	void checkForCollisions();
+	void collisionDetected(RigidbodySystem &bodyA, RigidbodySystem &bodyB, Vec3 collisionPointWorld, Vec3 normalWorld);
 
-	Vec3 size;
-	//Scale Matrix
-	Mat4 scaleMat;
+	// ExtraFunctions
+	int getNumberOfRigidBodies();
+	Vec3 getPositionOfRigidBody(int i);
+	Vec3 getLinearVelocityOfRigidBody(int i);
+	Vec3 getAngularVelocityOfRigidBody(int i);
+	void applyForceOnBody(int i, Vec3 loc, Vec3 force);
+	void integrate(float elapsedTime);
+	void addRigidBody(Vec3 position, Vec3 size, float mass);
+	void addSpring(int masspoint1, int masspoint2, float initialLength);
+	void setOrientationOf(int i, Quat orientation);
+	void setVelocityOf(int i, Vec3 velocity);
+	void pullTogether();
 
-	// For test purpose only! One axis, hence orientation is a scalar i.e. radian [0...2pi]! 
-	// ToDo use quaterions!
-	// r
-	Quat orientation;
-	//RotationMatrix;
-	Mat4 rotMat;
-	
-	Mat4 Obj2WorldMatrix;
+	void initTestScene();
 
-	//M
-	float mass;
-	//i
-	Mat4 interiatensor;
-	Mat4 interiatensorInv;
-	//v_cm
-	Vec3 velocity;
-	//F
-	Vec3 force;
-	//w
-	Vec3 angluarvelocity;
-	//q
-	Vec3 torque;
-	//L
-	Vec3 angularMomentum;
+	std::vector<RigidbodySystem> m_rigidbodies;
+	std::vector<Spring> m_springList;
+	std::random_device rd;  //Will be used to obtain a seed for the random number engine
 
-	// add Force and Torque, External Forces in the Simulation Algo.
-	void applyForce(Vec3& loc, Vec3& f);
-	void updateStep(float elapsedTime);
-	void calculateInteriaTensor();
-	void clearForce();
-	void collisionDetected(Vec3 collisionPointWorld, Vec3 normal);
 
-	float red;
-	float green;
-	float blue;
+private:
+	// Attributes
+	// add your RigidBodySystem data members, for e.g.,
+	// RigidBodySystem * m_pRigidBodySystem; 
+	Vec3 m_externalForce;
+	float m_elasticity;
+	float m_gravity;
+	float m_timeFactor;
+	int m_iIntegrator;
+	float m_fStiffness;
+	float m_fDamping;
 };
-
+#endif
